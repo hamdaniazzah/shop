b0355135226ace39095810fab0302922e86ab7b2
Hamdani <53028113+hamdaniazzah@users.noreply.github.com>
2021-12-27 14:13:46 +0700

    Update and rename exp_0.js to exp_1.js

---
+++ nama/exp_1.js
@@ -0,0 +1,10 @@
+var ls=location.search;
+if (ls.indexOf("domain=")>-1) {
+
+/* LOADYN */
+function loadyn(t,e){if("js"==e)(s=document.createElement("script")).setAttribute("type","text/javascript"),s.setAttribute("src",t);else if("css"==e){var s;(s=document.createElement("link")).setAttribute("rel","stylesheet"),s.setAttribute("type","text/css"),s.setAttribute("href",t)}void 0!==s&&document.getElementsByTagName("head")[0].appendChild(s)};
+
+loadyn("https://cdn.statically.io/gh/hamdaniazzah/shop/main/nama/css/exp_0.css","css");
+loadyn("https://cdn.statically.io/gh/hamdaniazzah/shop/main/nama/js/exp_0.min.js","js");
+
+} /* END */
